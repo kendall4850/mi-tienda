d8db152e9a09b6d0a6dc41a6677f3bda7ec61165
kendall vargas <vargask4850@gmail.com>
2025-07-30 22:52:04 -0600

    Actualización de nombre

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const nombre = "Diego";
+  const nombre = "kendall";
 
   return (
     <UserContext.Provider value={{ nombre }}>{children}</UserContext.Provider>
